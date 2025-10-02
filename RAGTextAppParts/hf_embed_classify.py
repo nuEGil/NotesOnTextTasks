0aa46afc060111f67e7cee973973c2142d012f88
nuEGil <42759839+nuEGil@users.noreply.github.com>
2025-10-02 14:26:31 -0500

    from distilbertmodel

--- RAGTextAppParts/hf_embed_classify.py
+++ RAGTextAppParts/hf_embed_classify.py
@@ -1,23 +1,30 @@
 from AC_pythonexample import * # will already have json and time imported  
 import torch 
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import DistilBertTokenizer, DistilBertModel
 '''
 distilebert tokenization and classification example 
 generic for interviews. 
 '''
-
 def load_model_and_tokenizer(name_ = 'distilbert-base-uncased'):
     tokenizer = DistilBertTokenizer.from_pretrained(name_)
-    model = DistilBertForSequenceClassification.from_pretrained(name_, num_labels = 2)
-    return model, tokenizer
+    model = DistilBertModel.from_pretrained(name_, num_labels = 2)
+    classifier = torch.nn.Linear(model.config.hidden_size, 2)
+    return model, tokenizer, classifier
 
-def parse_(text, model, tokenizer):
+def parse_(text, model, tokenizer, classifier):
+    
     inputs = tokenizer(text, return_tensors="pt") # tokenize the input
-    outputs = model(**inputs) # pass all tokenized inputs to the model
-    predicted_class = outputs.logits.argmax().item()
+    # forward pass through the model 
+    with torch.no_grad():
+        outputs = model(**inputs) # pass all tokenized inputs to the model
+        hidden_states = outputs.last_hidden_state # (batch_size, seq_len, hidden_dim)
+        avg_pool = hidden_states.mean(dim=1)
+    # pooled representation through classifier 
+    logits = classifier(avg_pool)
+    pred_class = torch.argmax(logits, dim = -1).item()
     
     print(f"\nText: '{text}'")
-    print(f"Predicted class: {predicted_class} (0 for negative, 1 for positive)")
+    print(f"Predicted class: {pred_class} (0 for negative, 1 for positive)")
 
 if __name__ == '__main__':
 
@@ -26,9 +33,9 @@ if __name__ == '__main__':
     text = GetText(input_file)
     # get sentences
     sentence_starts, sentence_ends, sentences = GetSentences0(text, offset = 0)
-    model,tokenizer = load_model_and_tokenizer()
+    model, tokenizer, classifier = load_model_and_tokenizer()
     
     for i in range(10):
         print('---')
-        parse_(sentences[100+i], model, tokenizer)
+        parse_(sentences[100+i], model, tokenizer, classifier)
 
